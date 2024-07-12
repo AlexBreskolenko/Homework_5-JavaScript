6d4a53d7340230ee4e3c20f1d45afae0a37e6b92
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-07-12 20:36:17 +0300

    Task 2 completed

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//****************Task 1
 const numbers = {
   keyin1: 1,
   keyin2: 2,
@@ -13,3 +14,34 @@ for (const key in numbers) {
     console.log(numbers[key]);
   }
 }
+
+//*******************Task 2
+const post = {
+  author: "John", // вывести этот текст
+  postId: 23,
+  comments: [
+    {
+      userId: 10,
+      userName: "Alex",
+      text: "lorem ipsum",
+      rating: {
+        likes: 10,
+        dislikes: 2, // вывести это число
+      },
+    },
+    {
+      userId: 5, // вывести это число
+      userName: "Jane",
+      text: "lorem ipsum 2", // вывести этот текст
+      rating: {
+        likes: 3,
+        dislikes: 1,
+      },
+    },
+  ],
+};
+
+console.log(post.author);
+console.log(post.comments[0].rating.dislikes);
+console.log(post.comments[1].userId);
+console.log(post.comments[1].text);
