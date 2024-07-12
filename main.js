37f30afdea4c83763acc7bb38855386d20f16bb5
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-07-12 21:29:35 +0300

    Task 4 completed

--- main.js
+++ main.js
@@ -69,3 +69,39 @@ products.forEach((elem) => {
 products.forEach((elem) => {
   console.log(elem.price);
 });
+
+//************Task 4
+const product = [
+  {
+    id: 3,
+    price: 127,
+    photos: ["1.jpg", "2.jpg"],
+  },
+  {
+    id: 5,
+    price: 499,
+    photos: [],
+  },
+  {
+    id: 10,
+    price: 26,
+    photos: ["3.jpg"],
+  },
+  {
+    id: 8,
+    price: 78,
+  },
+];
+//Задание 1
+const resultProduct = product.filter((elem) => {
+  if (elem.photos && elem.photos.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+});
+
+console.log(resultProduct);
+//Задание 2
+product.sort((a, b) => a.price - b.price);
+console.log(product);
